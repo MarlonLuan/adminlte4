2445ef15e627a979c6efe3145e6abb13df0cfc34
MarlonLuan <9463224+MarlonLuan@users.noreply.github.com>
2025-01-01 01:47:15 +0000

    build

--- dist/js/adminlte.js
+++ dist/js/adminlte.js
@@ -1,6 +1,6 @@
 /*!
  * AdminLTE v4.0.0-beta3 (https://adminlte.io)
- * Copyright 2014-2024 Colorlib <https://colorlib.com>
+ * Copyright 2014-2025 Colorlib <https://colorlib.com>
  * Licensed under MIT (https://github.com/ColorlibHQ/AdminLTE/blob/master/LICENSE)
  */
 (function (global, factory) {
